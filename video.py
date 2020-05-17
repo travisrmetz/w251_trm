b4b2962d56d04c8ac3d20bf9d08ed775984acdce
Travis Metz <travisrmetz@gmail.com>
2020-05-17 18:33:49 -0400

    made video stop

--- video.py
+++ video.py
@@ -19,8 +19,8 @@ while(True):
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    #cv2.imshow('frame',gray)
-    #cv2.waitKey(0)
+    cv2.imshow('frame',gray)
+    cv2.waitKey(0)
 
     # We don't use the color information, so might as well save space
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
